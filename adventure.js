f357714a428771cfed3147bbe5dbe2f8542323a2
sd.dev <sd.dev@outlook.com>
2020-05-06 16:00:48 +0100

    Added adventure game test

---
+++ adventure.js
@@ -0,0 +1,3 @@
+$("#controlPanel").click(function () {
+    $("#gameText").text("The screen looks damaged, it flickers and occasionally fades to black for a second or two. I need to find the control for the alarm")
+});
